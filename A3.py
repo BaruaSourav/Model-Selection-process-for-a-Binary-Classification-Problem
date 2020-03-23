2b734b4a65ed262b3d35ababdadfa5baaf0f7bb8
Sourav Barua <sourav.shr6@gmail.com>
2020-03-23 17:55:54 -0230

    Updating the python file, trying to get plot from gridsearchcv performance.

--- A3.py
+++ A3.py
@@ -5,7 +5,7 @@ import numpy as np
 import decimal 
 import scipy.stats as stats
 import sklearn.datasets as datasets
-
+from sklearn.svm import SVC
 #pd.set_option('display.max_colwidth', -1)
 
 def main():
@@ -14,9 +14,17 @@ def main():
     # reading test and train dataset from the names supplied on command line
     test_dataset = pd.read_csv(sys.argv[1],sep='\t')
     train_dataset = pd.read_csv(sys.argv[2],sep='\t')
-    # print("Test")
-    # print(test_dataset)
-    # print(train_dataset)
+    
+    traindata_targets = train_dataset['group']
+    traindata_features = train_dataset.loc[:,train_dataset.columns!='group']
 
+    X = traindata_features
+    y = traindata_targets
+
+    est = SVC(C=1000,kernel='linear',probability=True)
+    est.fit(X, y)
+    y_score = est.predict_proba(test_dataset)
+
+    
 if __name__== '__main__':
     main()
