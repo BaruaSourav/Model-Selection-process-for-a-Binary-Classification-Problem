06bf4d9cffbb584cf9c2bdb2cff7c50991453501
Sourav Barua <sourav.shr6@gmail.com>
2020-03-23 20:32:51 -0230

    Fina python file updated.

--- A3.py
+++ A3.py
@@ -2,9 +2,6 @@ import pandas as pd
 import csv
 import sys
 import numpy as np
-import decimal 
-import scipy.stats as stats
-import sklearn.datasets as datasets
 from sklearn.svm import SVC
 #pd.set_option('display.max_colwidth', -1)
 
@@ -12,19 +9,21 @@ def main():
     for args in sys.argv:
         print(str(args))
     # reading test and train dataset from the names supplied on command line
-    test_dataset = pd.read_csv(sys.argv[1],sep='\t')
-    train_dataset = pd.read_csv(sys.argv[2],sep='\t')
+    test_dataset = pd.read_csv(sys.argv[2],sep='\t')
+    train_dataset = pd.read_csv(sys.argv[1],sep='\t')
     
     traindata_targets = train_dataset['group']
     traindata_features = train_dataset.loc[:,train_dataset.columns!='group']
 
     X = traindata_features
     y = traindata_targets
-
+    print("Fitting the training data on SVC Method...")
     est = SVC(C=1000,kernel='linear',probability=True)
     est.fit(X, y)
+    print("Predicting confidence score for different groups...")
     y_score = est.predict_proba(test_dataset)
+    print("Writing the confidence predictions ")
+    y_score.to_csv("g13_predictions.txt", sep="\t", index=False,float_format='%.4f')
 
-    
 if __name__== '__main__':
     main()
